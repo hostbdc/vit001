da8729548cf68e8ba55256b68d6175c00b09996b
vanderlei vittrine <central@hostbdc.com.br>
2017-11-02 09:05:27 -0200

    test
    asssss

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={		
+	'ApiUrl':"http://delivery.vittrine.xyz/mobileapp/api",				
+	'DialogDefaultTitle':"DELIVERY Vittrine",	
+	'pushNotificationSenderid':"76287014202",	
+	'facebookAppId':"", 
+	'APIHasKey':"d6f56f1294b89151a8f01026d9aa3ac7"
+};
